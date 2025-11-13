b57718d926b91dd829a6c713325eb1c06f4e85d8
AlieeLinux <bazartroy531@gmail.com>
2025-11-13 17:01:53 +0800

    next version bump

--- testexception.py
+++
@@ -1,3 +0,0 @@
-from utils.Decryption import Error as err
-
-err("decryption")
--- writing.py
+++
@@ -1,9 +0,0 @@
-from utils.Encryption import NewJob
-import os
-
-fff = NewJob()
-
-fff.decryptpasswordwrite()
-
-os.system("vim passwords.csv")
-fff.encryptpasswords()
